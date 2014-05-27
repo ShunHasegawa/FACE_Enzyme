3f39fb96b60c372262a8fc11aac6bafd15a23768
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-27 20:45:02 +1000

    regression line plots are added

--- R/Figs.R
+++ R/Figs.R
@@ -20,7 +20,7 @@ fls <- paste("Output//Figs/FACE_Enzyme_CO2Trt_", vars, sep = "")
 l_ply(1:4, function(x) ggsavePP(filename = fls[x], plot = TrtFg[[x]], width = 6, height = 3))
 
 ##################################
-# plot all nutrient in one graph #
+# plot all enzymes in one graph #
 ##################################
 # labels for facet_grid
 ylab_label <- function(variable, value){
@@ -30,3 +30,19 @@ ylab_label <- function(variable, value){
 pl <- PltCO2Mean(TrtMean) +
   facet_wrap(~ variable, ncol = 2, scales= "free_y")
 ggsavePP(filename = "output//figs/FACE_Enzyme_CO2Trt", plot = pl, width = 6, height = 4)
+
+#########################################
+# plot regression agianst soil moisture #
+#########################################
+RegFg <- dlply(enzMlt,. (variable), pltReg) 
+fls <- paste("Output//Figs/FACE_Enzyme_RegreMoist_", vars, sep = "")
+l_ply(1:4, function(x) ggsavePP(filename = fls[x], plot = RegFg[[x]], width = 6, height = 3))
+
+pl <- pltReg(enzMlt) +
+  facet_wrap(~ variable, ncol = 2, scales= "free_y")
+ggsavePP(filename = "output//figs/FACE_Enzyme_RgreMoist", plot = pl, width = 6, height = 4)
+
+
+
+
+
