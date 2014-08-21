efc9c78eb3949624f678f61d1285b7b71318dff0
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-08-21 02:29:48 +0100

    fig for manuscript is crated

--- R/Figs.R
+++ R/Figs.R
@@ -31,6 +31,55 @@ pl <- PltCO2Mean(TrtMean) +
   facet_wrap(~ variable, ncol = 2, scales= "free_y")
 ggsavePP(filename = "output//figs/FACE_Enzyme_CO2Trt", plot = pl, width = 6, height = 4)
 
+#######################
+# Fig for publication #
+#######################
+# define graphic background
+science_theme <- theme(panel.grid.major = element_line(size = 0.2, color = "grey"), 
+                       axis.text.x  = element_text(angle=45, vjust= 1, hjust = 1),
+                       legend.position = c(.9, .9), 
+                       legend.title = element_blank())
+
+
+# change labels
+vars <- c("CBH", "BG", "NAG", "AP")
+df <- TrtMean
+df$variable <- factor(df$variable, 
+                      levels = c("cello.act", "gluco.act", "nag.act", "phos.act"), 
+                      labels = vars)
+
+p <- ggplot(df, aes(x = Date, y = Mean, group = co2))
+
+p2 <- p + geom_line(aes(linetype = co2)) + 
+  geom_errorbar(aes(ymin = Mean - SE, ymax = Mean + SE), 
+                width = 15, size = .3,
+                position = position_dodge(20)) + 
+  geom_point(aes(shape = co2, fill = co2), position = position_dodge(20)) +
+  labs(x = "Month", y = expression(Potential~enzyme~activity~(mu*mol~h^"-1"~g^"-1"))) +
+  geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), 
+             linetype = "dashed", col = "black") +
+  scale_x_date(breaks= date_breaks("2 month"),
+               labels = date_format("%b-%y"), 
+               limits = c(as.Date("2012-08-15"), as.Date("2013-06-15"))) +
+  scale_shape_manual(values = c(24, 21), labels = c("Ambient", expression(eCO[2]))) +
+  scale_fill_manual(values = c("black", "white"), 
+                    labels = c("Ambient", expression(eCO[2]))) +
+  scale_linetype_manual(values = c("solid", "dashed"), 
+                        labels = c("Ambient", expression(eCO[2]))) +
+  facet_wrap(~ variable, ncol = 2, scales= "free_y") +
+  science_theme
+ggsavePP(filename = "Output//Figs//FACE_Manuscript/FACE_Enzyme", plot = p2, 
+         width = 6, height = 5)
+
+
+
+# white-black figure
+WBFig <- function(data, ylab, facetLab = ylab_label, figTheme = science_theme){
+  return(p2)
+}
+
+
+
 #########################################
 # plot regression agianst soil moisture #
 #########################################
