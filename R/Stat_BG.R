d10b28b0f943a26aed2589b22559aead85f094a1
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2014-11-10 15:31:55 +0000

    ancova's done

--- R/Stat_BG.R
+++ R/Stat_BG.R
@@ -19,3 +19,64 @@ AnvF_BG
 plot(Fml)
 qqnorm(residuals(Fml))
 qqline(residuals(Fml))
+
+######################
+# CO2 x Moist x Temp #
+######################
+
+# Determine how many days to go back from the sampling dates to calculate soil
+# variables
+m1 <- LmrAicComp(ListDF = LstDF_SoilVar, 
+                 formula = formula(log(gluco.act) ~ co2 * (log(Moist) + Temp_Mean) + 
+                                     (1|block) + (1|ring) + (1|id)))
+
+aicDF <- m1$AICdf
+aicDF[which(aicDF$AICs == min(aicDF$AICs)), ]
+
+# 7-day gives the lowest AIC
+df <- m1$Data
+
+## check linearity agains soil variables
+
+# plot against soil varriable
+scatterplotMatrix(~ log(gluco.act) + log(Moist) + Temp_Mean, diag = "boxplot", df)
+
+# what if I use soil moisture at day of sampling
+M.fmoist <- lmer(log(gluco.act) ~ co2 * (log(moisture) + Temp_Mean) + (1|block) + (1|ring) + (1|id), data = df)
+AIC(M.fmoist)
+# no difference, so just use this as it's simpler
+
+# plot for each plot against soil variables
+print(xyplot(log(gluco.act) ~ log(moisture) | ring + plot, m1$Data, type = c("r", "p")))
+print(xyplot(log(gluco.act) ~ Temp_Mean | ring + plot, m1$Data, type = c("r", "p")))
+# looks fine
+
+## Analysis
+
+# The initial model is
+Iml_ancv <- M.fmoist
+Anova(Iml_ancv)
+
+# The final models
+# Fml_ancv <- stepLmer(Iml_ancv)
+
+# for some reasons stepLmer doesn't work with this so manually remove ns factors
+m2 <- lmer(log(gluco.act) ~ co2 * log(moisture) + Temp_Mean + 
+             (1|block) + (1|ring) + (1|id), data = df)
+m3 <- lmer(log(gluco.act) ~ co2 + log(moisture) + Temp_Mean + 
+             (1|block) + (1|ring) + (1|id), data = df)
+m4 <- lmer(log(gluco.act) ~ co2 + log(moisture) + 
+             (1|block) + (1|ring) + (1|id), data = df)
+m5 <- lmer(log(gluco.act) ~ log(moisture) + 
+             (1|block) + (1|ring) + (1|id), data = df)
+anova(Iml_ancv, m2, m3, m4, m5)
+
+Fml_ancv <- m5
+Anova(Fml_ancv)
+AnvF_ancv_BG <- Anova(Fml_ancv, test.statistic = "F")
+AnvF_ancv_BG
+
+plot(allEffects(Fml_ancv))
+plot(Fml_ancv)
+qqnorm(resid(Fml_ancv))
+qqline(resid(Fml_ancv))
--- R/Stat_CBH.R
+++ R/Stat_CBH.R
@@ -1,6 +1,6 @@
-#############
-# cello.act #
-#############
+##############
+# CO2 x Time #
+##############
 bxplts(value = "cello.act", data = enzy)
   # use sqrt transormation
 
@@ -20,3 +20,60 @@ plot(Fml)
 # little wedge-shaped
 qqnorm(residuals(Fml))
 qqline(residuals(Fml))
+
+######################
+# CO2 x Moist x Temp #
+######################
+
+# Determine how many days to go back from the sampling dates to calculate soil
+# variables
+m1 <- LmrAicComp(ListDF = LstDF_SoilVar, 
+                 formula = formula(log(cello.act) ~ co2 * (log(Moist) + Temp_Mean) + 
+                                     (1|block) + (1|ring) + (1|id)))
+
+aicDF <- m1$AICdf
+aicDF[which(aicDF$AICs == min(aicDF$AICs)), ]
+
+# 2-day gives the lowest AIC
+df <- m1$Data
+
+## check linearity agains soil variables
+
+# plot against soil varriable
+scatterplotMatrix(~ log(cello.act) + log(Moist) + Temp_Mean, diag = "boxplot", df)
+
+# what if I use soil moisture at day of sampling
+M.fmoist <- lmer(log(cello.act) ~ co2 * (log(moisture) + Temp_Mean) + (1|block) + (1|ring) + (1|id), data = df)
+AIC(M.fmoist)
+# this is slightly better than above, so use this
+
+# plot for each plot against soil variables
+print(xyplot(log(cello.act) ~ log(moisture) | ring + plot, m1$Data, type = c("r", "p")))
+print(xyplot(log(cello.act) ~ Temp_Mean | ring + plot, m1$Data, type = c("r", "p")))
+# looks fine
+
+## Analysis
+
+# The initial model is
+Iml_ancv <- M.fmoist
+Anova(Iml_ancv)
+
+# The final models
+# Fml_ancv <- stepLmer(Iml_ancv)
+
+# for some reasons stepLmer doesn't work with this so manually remove ns factors
+m2 <- lmer(log(cello.act) ~ co2 * log(moisture) + Temp_Mean + 
+             (1|block) + (1|ring) + (1|id), data = df)
+m3 <- lmer(log(cello.act) ~ co2 * log(moisture) + 
+             (1|block) + (1|ring) + (1|id), data = df)
+anova(Iml_ancv, m2, m3)
+
+Fml_ancv <- m3
+Anova(Fml_ancv)
+AnvF_ancv_CBH <- Anova(Fml_ancv, test.statistic = "F")
+AnvF_ancv_CBH
+
+plot(allEffects(Fml_ancv))
+plot(Fml_ancv)
+qqnorm(resid(Fml_ancv))
+qqline(resid(Fml_ancv))
