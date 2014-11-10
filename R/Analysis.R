7d664203e4209ad9f489b881ab884972c2ebb2f9
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2014-11-10 13:43:08 +0000

    date column is renamed as it's confusing

--- R/Analysis.R
+++ R/Analysis.R
@@ -12,7 +12,7 @@ enz <- read.csv("Data//FACE_enzyme.csv", colClasses= c("ring" = "factor", "plot"
 enz$co2 <- factor(ifelse(enz$ring %in% c(1, 4, 5), "elev", "amb"))
 
 # Date
-enz$Date <- as.Date(recode(enz$dates, "'dec12' = '2012-12-11'; 
+enz$date <- as.Date(recode(enz$dates, "'dec12' = '2012-12-11'; 
                                         'jun13' = '2013-06-11' ;
                                         'mar13' = '2013-03-11' ; 
                                         'sep12' = '2012-09-03'"))
@@ -20,14 +20,15 @@ enz$Date <- as.Date(recode(enz$dates, "'dec12' = '2012-12-11';
 enz[c("cello.act", "gluco.act", "nag.act", "phos.act")] <- enz[c("cello.act", "gluco.act", "nag.act", "phos.act")]/1000
 
 #plot mean
-enzy <- ddply(enz, .(dates, Date, co2, ring, plot), 
+enzy <- ddply(enz, .(dates, date, co2, ring, plot), 
               function(x) colMeans(x[c("cello.act", "gluco.act", "nag.act", "phos.act")], na.rm = TRUE))
 
 # soil variable
 soil <- read.table("Data//FACE_enzyme_SoilVar.txt", header = T, colClasses = c("ring" = "factor", "plot" = "factor"))
+names(soil)[1] <- "Date"
 
 # merge soil variable with plot mean enzyme data
-enzy <- merge(enzy, soil, by.x = c("dates", "ring", "plot"), by.y = c("date", "ring", "plot"))
+enzy <- merge(enzy, soil, by.x = c("dates", "ring", "plot"), by.y = c("Date", "ring", "plot"))
 
 # add block and id
 enzy <- within(enzy, {
