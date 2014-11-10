fbeaedc8231255e6922ef69da0aaa29cdc15f090
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2014-11-10 11:31:06 +0000

    functions to do statistics

--- R/functions.R
+++ R/functions.R
@@ -212,3 +212,68 @@ ggsavePP <- function(filename, plot, width, height){
 subsetD <-  function(...){
   droplevels(subset(...))
 }
+
+###########################
+# step deletion with lmer #
+###########################
+stepLmer <- function(model, red.rndm = FALSE, ddf = "Kenward-Roger", ...){
+  require(lmerTest)
+  update(step(model, reduce.random = red.rndm, ddf = ddf,...)$model, 
+         contrasts = NULL)
+}
+# use "Kenward-Roger" for approximation for denominator degrees of freedom. This
+# is the same as the default DF given by Anova(model, test.statistic = "F). The
+# default of step gives me a warning message for IEM-NO3 for some reasons (not
+# sure why.. so changed it.)
+
+###########################################
+# produce box plots with transformed data #
+###########################################
+# log OR sqrt OR power(1/3) OR inverse OR box-cox
+bxplts <- function(value, ofst = 0, data, ...){
+  data$y <- data[[value]] + ofst #ofst is added to make y >0
+  a <- boxcox(y ~ co2 * time, ..., data = data)
+  par(mfrow = c(2, 3))
+  boxplot(y ~ co2*time, data, main = "raw")
+  boxplot(log(y) ~ co2*time, main = "log", data)
+  boxplot(sqrt(y) ~ co2*time, main = "sqrt", data)
+  boxplot(y^(1/3) ~ co2*time, main = "power(1/3)", data)
+  boxplot(1/y ~ co2*time, main = "inverse", data)
+  BCmax <- a$x[a$y == max(a$y)]
+  texcol <- ifelse(BCmax < 0, "red", "black") 
+  boxplot(y^(BCmax) ~ co2*time, 
+          main = "", sep = "=", 
+          data = data)
+  title(main = paste("Box Cox", round(BCmax, 4)), 
+        col.main = texcol)
+  par(mfrow = c(1,1))
+}
+
+# multiple box-cox power plot for different constant values
+bxcxplts <- function(value, data, sval, fval, ...){
+  data$yval <- data[[value]]
+  ranges <- seq(sval, fval, (fval - sval)/9)
+  
+  # store parameters given from box-cox plot
+  par(mfrow = c(5, 2))
+  BCmax <- vector()
+  for (i in 1:10){
+    data$y <- data$yval + ranges[i]
+    a <- boxcox(y ~ co2 * time, data = data, ...)
+    BCmax[i] <- a$x[a$y == max(a$y)]
+  }
+  
+  # plot box plot with poer given from box-box for 
+  # each contstant value
+  par(mfrow = c(5, 2))
+  par(omi = c(0, 0, 0, 0), mai = c(0.4, 0.4, 0.4, 0))
+  sapply(1:10, function(x) {
+    boxplot((yval + ranges[x]) ^ BCmax[x] ~ co2 * time, 
+            main = "", data = data)
+    texcol <- ifelse(BCmax[x] < 0, "red", "black") 
+    title(main = paste("constant=", round(ranges[x], 4), 
+                       ", boxcox=", round(BCmax[x], 4)),
+          col.main = texcol)
+  })
+  par(mfrow = c(1,1))
+}
